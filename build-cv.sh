a8cfb6644ba33ff68ee72f458d1a4ce46e8e2d3a
Konstantin Läufer <klaeufer@gmail.com>
2022-01-14 20:27:41 -0600

    added build script
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

---
+++ build-cv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -d ~/.linuxbrew ]; then
+  PATH=/usr/bin:$PATH
+fi
+
+NAME=laufer
+MAIN=${NAME}-cv
+DOMAIN=${NAME}.cs.luc.edu
+
+# Each of my BibTeX entries is kept in a separate file and assembled into one (for each type of publication).
+# Makes it easier to keep up-to-date.
+# Every entry in the .bib MUST have a year to sort properly. (We don't rigidly sort by other fields, month, day, since these don't always appear).
+
+pushd bibs >& /dev/null
+bash gather.sh
+popd >& /dev/null
+
+sleep 5
+
+latexmk -output-directory="./build" -C -pdf ${MAIN}.tex
+latexmk -output-directory="./build" -pdf ${MAIN}.tex
+
+if [ -f build/${MAIN}.pdf ]; then
+  if [ -d ../${DOMAIN} ]; then
+     cat bibliography/*.bib > ../${DOMAIN}/_bibliography/papers.bib
+     echo "Copied latest bibliography to website folder"
+  fi 
+fi
